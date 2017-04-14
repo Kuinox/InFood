4e18241a2c1af78d2c1d34081c8ebb630be36791
sirine <sassi@intechinfo.fr>
2017-04-14 12:09:19 +0200

    début index et inscription

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>video</title>
+	</head>
+	<body>
+		<form action="inscription.php" method="get">
+		<td>
+			<tr>
+				<input type="hidden"/>
+				<input type="submit" name="Inscription"/>
+			</tr>
+			<tr>
+				<input type="hidden"/>
+				<input type="submit" name="Se connecter"/>
+			</tr>
+		</td>
+		</form>
+	</body>
+</html>
