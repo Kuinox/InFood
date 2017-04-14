272d41eac186dfcd3cd65733e6095e9de7575810
Deeeeenis <denis.audinet@gmail.com>
2017-04-14 16:58:08 +0200

    insert user complet

--- insert.sql
+++ insert.sql
@@ -11,3 +11,26 @@ values
 ('michel','michel','michel@gmail.com','165','65'),
 ('pierre','pierre','pierre@gmail.com','185','65'),
 ('jaques','jaques','jaques@gmail.com','155','65');
+insert into grade (id_grade,name_grade)
+values
+(0,'utilisateur'),
+(1,'admin');
+insert into grade_user (user_id_user,grade_id_grade)
+values
+('toto',0),
+('titi',0),
+('jean',0),
+('paul',0),
+('steve',0),
+('nicolas',1),
+('tom',0),
+('michel',0),
+('pierre',0),
+('jaques',0);
+/* attendre la majj de la datab
+insert into unwanted_aliment (aliment_id_aliment,user_id_user)
+values
+(5,'toto'),
+(3,'pierre'),
+(2,'jaques');
+*/
