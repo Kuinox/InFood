521f7c299391aa8268b9ad44bc439a4225b3362f
Kuinox <nvandeginste@gmail.com>
2017-04-14 14:25:41 +0200

    recupère les données des produits.

--- maj_bdd.php
+++ maj_bdd.php
@@ -1,15 +1,43 @@
 <?php
-//lecture d'un fichier caractère par caractère
-//$fic=fopen('D:\Downloads\en.openfoodfacts.org.products.csv', "r");
-$fic=fopen('http://world.openfoodfacts.org/data/en.openfoodfacts.org.products.csv', "r");
-$i=0;
-while($i<100) {
-    $i++;
-    $caractere=fgetc($fic);
-    if(!feof($fic)) {
-        echo $caractere . "<br />";
+/****************************************************
+ * INIT                                             *
+ ****************************************************/
+function getWord($ressource) { //get a word from
+    $word = "";
+    while(true) {
+        $char = fgetc($ressource);
+        if($char == "\t" || $char == "\n") {
+            return [$word,$char];
+        }
+        $word .= $char;
     }
 }
+function getLine($ressource) {
+    $line = [];
+    while(true) {
+        $word_result = getWord($ressource);
+        if ($word_result[1] == "\n") {
+            return $line;
+        }
+        $line[] = $word_result[0];
+    }
+}
+function getProduct($ressource, $columns) {
+    $line = getLine($ressource);
+    $product = [];
+    foreach ($columns as $key=>$value) {
+        $product[$value] = $line[$key];
+    }
+    return $product;
+}
+/****************************************************
+ * Main                                             *
+ ****************************************************/
+
+$fic=fopen('http://world.openfoodfacts.org/data/en.openfoodfacts.org.products.csv', "r");
+$columns = getLine($fic);
+var_dump(getProduct($fic, $columns));
+
 fclose($fic) ;
 
 
