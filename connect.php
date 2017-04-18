5cb8882fd7bc9e2a018c2217082494e1aa07fad3
Kuinox <nvandeginste@gmail.com>
2017-04-18 16:23:45 +0200

    Passage au MySQLi

--- connect.php
+++ connect.php
@@ -1,4 +1,3 @@
 <?php
-@mysql_connect('localhost','root','') or die("Erreur d'accès à la BDD.");
-@mysql_select_db('infood') or die('Echec de sélection de la base.');
+$bdd = @mysqli_connect('localhost','root','','infood') or die("Erreur d'accès à la BDD.");
 ?>
