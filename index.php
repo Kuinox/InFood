9b8de52be586d205d3af58f61f8eb5c90f222b4e
sirine <sassi@intechinfo.fr>
2017-04-20 16:44:40 +0200

    pas terminer

--- index.php
+++ index.php
@@ -2,33 +2,35 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" href="style.css"/>
 		<title>INDEX</title>
 	</head>
 	<body>	
 		<h1>In'Food	</h1>
-		<button> inscription</button>
+		<form action="inscription1.php" method="get">
+			<input type="hidden"/>
+			<input type="submit" name="inscription" value="Inscription"/>
+		</form>
 		<form action="connexion.php" method="get">
 			<input type="hidden"/>
 			<input type="submit" name="connexion" value="Se connecter"/>
-			</form>		
+		</form>		
 		<form action="resultat_de_recherche.php" method="get">
 			<input type="text" name="recherche"/>
 			<input type="submit" name="recherche" value="recherche"/><br>
 			Filtre option : 
-			<select name="filtre" form="recherche">
-			  <option value="produit">Produit</option>
-			  <option value="qualite">qualite</option>
-			  <option value=""></option>
-			  <option value=""></option>
-			</select>
+		<select name="filtre" form="recherche">
+		  <option value="produit">Produit</option>
+		  <option value="qualite">qualite</option>
+		  <option value=""></option>
+		  <option value=""></option>
+		</select>
 			Filtre option : 
-			<select name="filtre" form="recherche">
-			  <option value="produit">Produit</option>
-			  <option value="qualite">qualite</option>
-			  <option value=""></option>
-			  <option value=""></option>
-			</select>
+		<select name="filtre" form="recherche">
+		  <option value="produit">Produit</option>
+		  <option value="qualite">qualite</option>
+		  <option value=""></option>
+		  <option value=""></option>
+		</select>
 			<div></div>
 		</form>		
 	</body>
--- inscription.php
+++ inscription.php
@@ -1,10 +1,59 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Inscription</title>
-	</head>
-	<body>
-		hello
-	</body>
-</html>
+<?php
+$conn=mysqli_connect('localhost','root','','inscription')or die(mysqli_error());
+$nom=$_COOKIE["nom"];
+$pre=$_COOKIE["pre"];
+$eml=$_COOKIE["eml"];
+$pwd=$_COOKIE["pwd"];
+/*$nom=$_POST['nom'];
+$pre=$_POST['pre'];
+$eml=$_POST['eml'];
+$pwd=$_POST['pwd'];
+$userchek=$eml;
+$userchek="SELECT * FROM users WHERE 'email'=$userchek";
+$res=mysqli_query($conn,$userchek);
+$yes=count($res);
+echo $yes;
+if($yes>0)
+{ 
+	// header('Location:/INFOOD/inscription1.php');
+	echo"erreur mail existe";
+}else{
+ // $username = mysqli_real_escape_string($conn,$_POST['nom']);
+
+  // $result = mysqli_query($conn, "SELECT * FROM users WHERE nom='".$username."' AND activate='0'");
+
+  // if ($result->num_rows){
+// echo "you are activated !!";
+ // }
+
+   // else
+  // {
+	  // echo "you are not activated !!";
+
+  // do what u like here if not activated
+    // }
+		
+	*/
+	$req="INSERT INTO users (nom, prenom, email, password) values ('$nom','$pre','$eml','$pwd')";
+	$res=mysqli_query($conn,$req);
+	
+
+?>
+<body>
+	<p>Bienvenue <?php echo($nom)?> votre compte à étés bien enregitrer</p>
+	<table>
+		<tr>
+			<td>Nom :</td>
+			<td><?php echo($nom)?></td>
+		</tr>
+		<tr>
+			<td>prenom :</td>
+			<td><?php echo($pre)?></td>
+		</tr>
+		<tr>
+			<td>Email :</td>
+			<td><?php echo($eml)?></td>
+		</tr>
+	</table>
+	<p><a href="index.php">Retour à la page d'accueil </a></p>
+</body>
