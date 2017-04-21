46fe66bf3582cb25147f1374b6ff272f0f66ced4
Deeeeenis <denis.audinet@gmail.com>
2017-04-21 14:32:18 +0200

    nouveau

--- insert.sql
+++ insert.sql
@@ -30,3 +30,10 @@ values
 
 INSERT INTO grade_nutriment (tag_grade_nutriment)
 VALUES ('A'),('B'),('C'),('D'),('E'),('F');
+
+-- attendre la majj de la datab
+--insert into unwanted_aliment (aliment_id_aliment,user_id_user)
+--values
+--(5,'toto'),
+--(3,'pierre'),
+--(2,'jaques');
