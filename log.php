b76aad0d1f0ea2a2b868c10256bf412ff50e7100
Deeeeenis <denis.audinet@gmail.com>
2017-04-21 16:16:15 +0200

    renomage de variable string = information

--- log.php
+++ log.php
@@ -1,5 +1,5 @@
 <?php
-function logo($file,$string) {
-    file_put_contents($file , $string."\n" , FILE_APPEND);
+function logo($file,$informations) {
+    file_put_contents($file , $informations."\n" , FILE_APPEND);
 }
 ?>
