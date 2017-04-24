05642334c7348563a30f592053c58ea2b9004ab3
Deeeeenis <denis.audinet@gmail.com>
2017-04-24 12:16:32 +0200

    rework mdc

--- insert.sql
+++ insert.sql
@@ -1,32 +1,32 @@
 USE infood;
-INSERT INTO user (id_user,password,email,height,weight)
+INSERT INTO user (id_user,pseudo,password,email,height,weight)
 values
-('toto','toto','toto@gmail.com','175','70'),
-('titi','titi','titi@gmail.com','185','80'),
-('jean','jean','jean@gmail.com','165','65'),
-('paul','paul','paul@gmail.com','155','65'),
-('steve','steve','steve@gmail.com','178','75'),
-('nicolas','nicolas','nicolas@gmail.com','175','80'),
-('tom','tom','tom@gmail.com','145','39'),
-('michel','michel','michel@gmail.com','165','65'),
-('pierre','pierre','pierre@gmail.com','185','65'),
-('jaques','jaques','jaques@gmail.com','155','65');
+(NULL,'toto','toto','toto@gmail.com','175','70'),
+(NULL,'titi','titi','titi@gmail.com','185','80'),
+(NULL,'jean','jean','jean@gmail.com','165','65'),
+(NULL,'paul','paul','paul@gmail.com','155','65'),
+(NULL,'steve','steve','steve@gmail.com','178','75'),
+(NULL,'nicolas','nicolas','nicolas@gmail.com','175','80'),
+(NULL,'tom','tom','tom@gmail.com','145','39'),
+(NULL,'michel','michel','michel@gmail.com','165','65'),
+(NULL,'pierre','pierre','pierre@gmail.com','185','65'),
+(NULL,'jaques','jaques','jaques@gmail.com','155','65');
 INSERT INTO grade (id_grade,name_grade)
 values
-(0,'utilisateur'),
+(2,'utilisateur'),
 (1,'admin');
 INSERT INTO grade_user (user_id_user,grade_id_grade)
 values
-('toto',0),
-('titi',0),
-('jean',0),
-('paul',0),
-('steve',0),
-('nicolas',1),
-('tom',0),
-('michel',0),
-('pierre',0),
-('jaques',0);
+(1,0),
+(2,0),
+(3,0),
+(4,0),
+(5,0),
+(6,1),
+(7,0),
+(8,0),
+(9,0),
+(10,0);
 
 INSERT INTO grade_nutriment (tag_grade_nutriment)
 VALUES ('A'),('B'),('C'),('D'),('E'),('F');
