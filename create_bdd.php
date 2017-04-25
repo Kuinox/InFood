ad428fcdf7a2fad59c8b4158fd532666cabf609d
Kuinox <nvandeginste@gmail.com>
2017-04-25 16:01:35 +0200

    WIP pour le nouveau code de SQL

--- create_bdd.php
+++ create_bdd.php
@@ -6,8 +6,6 @@ header( 'content-type: text/html; charset=utf-8' );
 include("csv_functions.php");
 include("sql_functions.php");
 include("connect.php");
-
-
 set_time_limit(3000);
 
 sqlScriptInject($bdd,'create.sql');
