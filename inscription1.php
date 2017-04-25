386afa3874f6fb3318d2dc36409e6973f1f4c17b
Kuinox <nvandeginste@gmail.com>
2017-04-25 14:33:37 +0200

    jenesaispascequ'ilsepasse

--- inscription1.php
+++ inscription1.php
@@ -22,49 +22,20 @@ if(isset($_POST['forminscription'])){
 		echo "Mot de passe est vide";
 	}
 	else
-	{
+	{	
+		$options = [
+			'cost' => 12,
+		];
+		$mm=password_hash($_POST["pwd"], PASSWORD_BCRYPT, $options);
 		setcookie("nom", $_POST["nom"], time()+3600*12);
 		setcookie("eml", $_POST["eml"], time()+3600*12);
-		setcookie("pwd", $_POST["pwd"], time()+3600*12);
+		setcookie("pwd", $mm, time()+3600*12);
+		// setcookie("pwd", $m, time()+3600*12);
 		setcookie("height", $_POST["height"], time()+3600*12);
 		setcookie("weight", $_POST["weight"], time()+3600*12);
 		header('Location:/INFOOD/inscription.php');
 	}
 }
+$html=include("inscriptionvu.html");
+echo"$html";
 ?>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Inscription</title>
-	</head>
-	<body>
-	<form method="POST" action="inscription1.php">
-		<table>		
-			<tr>
-				<td>Nom :</td>
-				<td><input type="text" name="nom"/></td>
-			</tr>
-			<tr>
-				<td>poid :</td>
-				<td><input type="text" name="weight"/></td>
-			</tr>
-			<tr>
-				<td>taille en centimetre :</td>
-				<td><input type="text" name="height"/></td>
-			</tr>
-			<tr>
-				<td>Mail :</td>
-				<td><input type="email" name="eml"/></td>
-			</tr>
-			<tr>
-				<td>Mot de Passe :</td>
-				<td><input type="password" name="pwd"/></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td><input type="submit" name="forminscription" value="Inscription"/></td>
-			</tr>
-		</table>
-	</form>
-	</body>
-</html>
---
+++ inscriptionreussie.php
@@ -0,0 +1,14 @@
+<body>
+	<p>Bienvenue $nom votre compte à étés bien enregitrer</p>
+	<table>
+		<tr>
+			<td>Nom :</td>
+			<td>$nom</td>
+		</tr>
+		<tr>
+			<td>Email :</td>
+			<td>$eml</td>
+		</tr>
+	</table>
+	<p><a href='index.php'>Retour à la page d accueil </a></p>
+</body>
