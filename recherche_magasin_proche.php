ddb51c03c17ab2f3c4d2c1840ef14e09a705b848
sirine <sassi@intechinfo.fr>
2017-05-02 16:19:05 +0200

    essai

---
+++ recherche_magasin_proche.php
@@ -0,0 +1,7 @@
+<iframe
+  width="600"
+  height="450"
+  frameborder="0" style="border:0"
+  src="https://www.google.com/maps/embed/v1/place?key= AIzaSyByt66nAk8JGLHvJ6ZJbNYp9SVhAsE0-Mc 
+    &q=Space+Needle,Seattle+WA" allowfullscreen>
+</iframe>
