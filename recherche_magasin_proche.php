9ced9b3ee0cefd83c088a65cd7f057381853a35c
sirine <sassi@intechinfo.fr>
2017-05-04 09:20:51 +0200

    recherche magasin pas terminer

--- recherche_magasin_proche.php
+++ recherche_magasin_proche.php
@@ -1,7 +1,8 @@
 <iframe
-  width="600"
-  height="450"
-  frameborder="0" style="border:0"
+  width="300";
+  height="300";
+  frameborder="0" style="border:0";
   src="https://www.google.com/maps/embed/v1/place?key= AIzaSyByt66nAk8JGLHvJ6ZJbNYp9SVhAsE0-Mc 
-    &q=Space+Needle,Seattle+WA" allowfullscreen>
+    &q=Eiffel+Tower,Paris+France" allowfullscreen>
+
 </iframe>
--- user.php
+++ user.php
@@ -1,7 +1,5 @@
  <?php 
  session_start();
 $nom=$_SESSION['nom'];
-
- 
 echo"<h1>bonjour $nom</h1>"
 ?>
