6b781bf56f003a4bdf74867329c7f3f65528a985
Deeeeenis <denis.audinet@gmail.com>
2017-05-09 15:59:50 +0200

    com

---
+++ ajouter_comment.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+include('connect.php');
+include('comments.php');
+
+$text = $_POST['comment'];
+ $id = $_SESSION["id"];
+ $filtre = $_SESSION["filtre"];
+ $id_user = $_SESSION['id_user'];
+add_comments($bdd,$text,$id,$id_user);
+header("location:produit.php?id=$id&filtre=$filtre");
+
+
+
+
+ ?>
