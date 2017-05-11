0a9871746f9bbf3ee6d4dac4497790e32be85f1c
sirine <sassi@intechinfo.fr>
2017-05-11 22:15:13 +0200

    mirror master/ sirine a push autre part que sur le dev

--- recherche_dans_bdd.php
+++ recherche_dans_bdd.php
@@ -1,9 +1,28 @@
 <?php 
+<<<<<<< HEAD
 // include("controller/SQL/FUNCTIONS/connect.php");
 $bdd = new PDO('localhost','root','','infood');
 $requete = $bdd->query("SELECT * FROM user");
+=======
+// include("connect.php");
+ /*session_start();
+$email =$_SESSION['email'];
+$bdd = new PDO('mysql:dbname=infood;host=localhost','root','');
+$requete = $bdd->query("SELECT  pseudo  FROM user WHERE email like 's@s'");
+>>>>>>> a64cdff... inscription
 while($data = $requete->fetch()){
 echo'<h2>'.$data['pseudo'].'</h2><br>';
+		$_SESSION['name']=$data['pseudo']};*/
+		*session_start();
+$email =$_SESSION['email'];
+$bdd = new PDO('mysql:dbname=infood;host=localhost','root','');
+$requete = $bdd->query("SELECT  pseudo  FROM user WHERE email like 's@s'");
+var_dump($requete);
+while($data = $requete->fetch()){
+echo'<h2>'.$data['pseudo'].'</h2><br>';
+		$_SESSION['name']=$data['pseudo'];
+		
+
 }
 /*$reponce = mysql_query("SELECT * FROM user WHERE pseudo LIKE 'toto'");
 while($donnees = mysql_fetch_array($reponse))
