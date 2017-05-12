79889fc575ee2c9203026082a2bee66f19c568b6
sirine <sassi@intechinfo.fr>
2017-05-12 14:24:46 +0200

    mirror master/ sirine a push autre part que sur le dev

--- recherche_dans_bdd.php
+++ recherche_dans_bdd.php
@@ -1,5 +1,6 @@
 <?php 
 <<<<<<< HEAD
+<<<<<<< HEAD
 // include("controller/SQL/FUNCTIONS/connect.php");
 $bdd = new PDO('localhost','root','','infood');
 $requete = $bdd->query("SELECT * FROM user");
@@ -19,14 +20,16 @@ $bdd = new PDO('mysql:dbname=infood;host=localhost','root','');
 $requete = $bdd->query("SELECT  pseudo  FROM user WHERE email like 's@s'");
 var_dump($requete);
 while($data = $requete->fetch()){
+=======
+session_start();
+// $email =$_SESSION['email'];
+$bdd = new PDO('mysql:dbname=infood;host=localhost','root','');
+$requete = $bdd->query("SELECT  pseudo  FROM user WHERE email like 's@s'");
+// var_dump($requete);
+while($data = $requete->fetch())
+{
+>>>>>>> d369597... commit to switch
 echo'<h2>'.$data['pseudo'].'</h2><br>';
 		$_SESSION['name']=$data['pseudo'];
-		
-
 }
-/*$reponce = mysql_query("SELECT * FROM user WHERE pseudo LIKE 'toto'");
-while($donnees = mysql_fetch_array($reponse))
-{
-	echo $donnees['pseudo'] . '<br />';
-}*/
 ?>
