7c291efb513efc54fff892979223777a5b9a898d
sirine <sassi@intechinfo.fr>
2017-05-12 16:00:17 +0200

    changement de branche

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 		<h1>In'Food	</h1>
 		<form action="inscription1.php" method="get">
 			<input type="hidden"/>
-			<input type="submit" name="inscription" value="Inscription"/>
+			
+<a href="pourpouup.php"  onclick="open('connectionessai.php', 'Popup', 'scrollbars=1,resizable=1,height=560,width=770'); return false;" ><input type="submit" name="inscription" value="Inscription"/></a><br />
 		</form>
 		<form action="connectionessai.php" method="get">
 			<input type="hidden"/>
