e5eaaef8b772228efd91607c795b7e04e066ba5b
Deeeeenis <denis.audinet@gmail.com>
2017-05-18 14:29:27 +0200

    update

--- index.php
+++ index.php
@@ -9,11 +9,7 @@
 		<?php include("view/header.html"); ?>
 		<h1>In'Food	</h1>
 		<form action="inscription1.php" method="get">
-			<input type="hidden"/>
-<<<<<<< HEAD
 
-=======
->>>>>>> 52bc534b3c34a3c1e9a27c58318b052cf229e3f4
 <a href="pourpouup.php"  onclick="open('connectionessai.php', 'Popup', 'scrollbars=1,resizable=1,height=560,width=770'); return false;" ><input type="submit" name="inscription" value="Inscription"/></a><br />
 		</form>
 		<form action="connectionessai.php" method="get">
@@ -22,11 +18,9 @@
 		</form>
 		<form action="resultat_de_recherche.php" method="GET" >
 			<input type="text" name="recherche"/>
-<<<<<<< HEAD
-		<select name= "type">
-=======
+
 		<select name="type">
->>>>>>> 52bc534b3c34a3c1e9a27c58318b052cf229e3f4
+
 		  <option value="aliment">Produit</option>
 			<option value="additive">additif</option>
 			<option value="ingredient">ingredient</option>
--- produit.php
+++ produit.php
@@ -5,18 +5,13 @@
 <?php
 session_start();
 include('comments.php');
-
 include("controller/SQL/FUNCTIONS/connect.php");
-
 include ('sql_functions.php');
-<<<<<<< HEAD
+
+
 $table = $_GET['type'];
-=======
 // include ("aj_panier.html");
 // if(isset($_POST['forminscription'])){
-
-$table = $_GET['filtre'];
->>>>>>> 52bc534b3c34a3c1e9a27c58318b052cf229e3f4
 $where = "id_aliment";
 $like = "'".$_GET['id']."'";
 $_SESSION["type"] = $table;
@@ -27,13 +22,7 @@ foreach ($result as $key => $value) {
   $chaine = implode(";", $value);
   $array = explode (";",$chaine);
   echo $array[1];
-<<<<<<< HEAD
 
-=======
-  echo "<br>";
-  echo $array[0];
-//Panier
->>>>>>> 52bc534b3c34a3c1e9a27c58318b052cf229e3f4
     $_SESSION["prod"] = $array[1];
     $_SESSION["id"] = $array[0];
 
@@ -44,7 +33,7 @@ foreach ($result as $key => $value) {
 //sirine
 
 $like = $_SESSION['id'];
-<<<<<<< HEAD
+
 $com = comments($bdd,$like);
 // A mettre en forme
 var_dump($com);
@@ -52,16 +41,11 @@ $note_moy = notes($bdd,$like);
 var_dump($note_moy);
 
 echo "<br><a href = notes.html>Notes</a>";
-echo "<br><a href = produit.html>Commenter</a>";
-//==================
-=======
-$test = comments($bdd,$like);
-var_dump($test);
+echo "<br><a href = produit.html>Commenter</a><br>";
+
 }if(isset($_SESSION["nom"])){
 	echo"set ************************************s";
 }else{
 	echo"not set n,;:!ùosqjfghjhfh";
 }
->>>>>>> gerer_panier
-
  ?>
