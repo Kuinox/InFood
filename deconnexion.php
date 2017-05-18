cc9866d194a789d6c16153ecab45e2924877a821
Deeeeenis <denis.audinet@gmail.com>
2017-05-18 19:40:14 +0200

    deco

--- deconnexion.php
+++ deconnexion.php
@@ -1,13 +1,10 @@
 <?php
-function deconnexion () {
+
   session_start();
   session_destroy();
     $informations = Array(/*Déconnexion*/
   				'Déconnexion',
   				'Vous êtes à présent déconnecté.');
-  exit();
-  return $informations;
-  header('Location:/INFOOD/index.php');
-}
-deconnexion();
+
+header('Location:/INFOOD/index.php');
 ?>
