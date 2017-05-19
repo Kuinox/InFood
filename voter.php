4470fa4078e2449555387cf62195313793a60fb6
Deeeeenis <denis.audinet@gmail.com>
2017-05-19 16:00:26 +0200

    vo

--- voter.php
+++ voter.php
@@ -7,6 +7,9 @@ $note = $_POST['vote'];
 $id = $_SESSION["id"];
 $type = $_SESSION["type"];
 $id_user = $_SESSION["id_user"];
+
+
+if (is_int($note)){
 if ($note <= 5){
     add_note($bdd,$note,$id,$id_user);
     header("location:produit.php?id=".$id."&type=".$type."");
@@ -14,7 +17,8 @@ if ($note <= 5){
     echo "la note doit etre entre 0 et 5 ";
     echo "<br><a href = index>retour</a>";
 }
-
-
+}else{
+  echo "sa doit etre un chiffre";
+}
 
 ?>
