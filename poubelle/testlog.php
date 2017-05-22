dca5e0a2e6f9087e12d4c040d0eef37c3ad0a3f4
Kuinox <nvandeginste@gmail.com>
2017-05-23 00:53:31 +0200

    hotfix d'un mauvais déplacement de répertoire, poubelle supprimé.

--- poubelle/testlog.php
+++
@@ -1,7 +0,0 @@
-<?php
-$file = "logo.txt";
-$string = "hey salut les teriens";
-include ("log.php");
-$test= "logo";
-$test($file,$string);
-?>
--- poubelle/user.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo"<h1>bonjour ".$_SESSION['nom']."</h1>";
-include("index.php");
-?>
-<a href="./deconnexion.php">déconnexion</a>
