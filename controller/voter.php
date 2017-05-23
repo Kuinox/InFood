b5985d64d140de8e65c3e241d4b224679cc98087
Deeeeenis <denis.audinet@gmail.com>
2017-05-23 09:33:25 +0200

    modif com et note en cours

--- controller/voter.php
+++ controller/voter.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
-include("../controller/SQL/FUNCTIONS/connect.php");
-include('../controller/SQL/FUNCTIONS/comments.php');
+include("SQL/FUNCTIONS/connect.php");
+include('SQL/FUNCTIONS/comments.php');
 
 $note = $_POST['vote'];
 $id = $_SESSION["id"];
 $type = $_SESSION["type"];
-$id_user = $_SESSION["id_user"];
+$id_user = $_SESSION['user']['id_user'];
 if ($note <= 5){
     add_note($bdd,$note,$id,$id_user);
-    header("location:../model/produit.php?id=".$id."&type=".$type."");
+    header("location:../aliment.php?id=".$id."&type=".$type."");
 }else {
     echo "la note doit etre entre 0 et 5 ";
     echo "<br><a href = index>retour</a>";
