3c86cfe02b48d04a505cb3a2f7ce92679f8233d0
Deeeeenis <denis.audinet@gmail.com>
2017-05-23 14:38:25 +0200

    update note

--- controller/voter.php
+++ controller/voter.php
@@ -8,8 +8,9 @@ $id = $_SESSION["id"];
 $type = $_SESSION["type"];
 $id_user = $_SESSION['user']['id_user'];
 if ($note <= 5){
-    add_note($bdd,$note,$id,$id_user);
-    header("location:../aliment.php?id=".$id."&type=".$type."");
+  mysqli_query($bdd, "CALL update_note($id,$note,$id_user)")
+  //  add_note($bdd,$note,$id,$id_user);
+  //  header("location:../aliment.php?id=".$id."&type=".$type."");
 }else {
     echo "la note doit etre entre 0 et 5 ";
     echo "<br><a href = index>retour</a>";
