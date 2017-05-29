9c187a26af9bbc1b7aba4f1014a5ddbad102423a
sirine <sassi@intechinfo.fr>
2017-05-29 14:12:24 +0200

    no message

--- aliment.php
+++ aliment.php
@@ -1,6 +1,5 @@
 <?php
 include_once("controller/SQL/FUNCTIONS/connectNoUse.php");
-
 if($db_exist) {
  ?>
 
@@ -13,12 +12,14 @@ if($db_exist) {
         include("controller/SQL/FUNCTIONS/connect.php");
         include("controller/SQL/FUNCTIONS/select.php");
         include ("controller/TEST.php");
+        include ("controller/fonction_ajout_panier.php");
         //include ('sql_functions.php');
 
       echo "<pre>";
       print_r (select ($bdd, "aliment", "id_aliment",$_GET['id']));
       echo "</pre>";
       displayComents();
+	  ajout_panier()
         ?>
 	</body>
 </html>
--- controller/functions/fonction_ajout_panier.php
+++ controller/functions/fonction_ajout_panier.php
@@ -1,32 +1,27 @@
 <?php
-function ajout_panier($_GET['id'])
+function ajout_panier()
 {
-	if(isset($_POST['go']))
+	if(!isset($_SESSION['panier']))
 	{
-		if(!isset($_SESSION['panier']))
-		{
-			//créer panier si n'existe pas
-			$_SESSION['panier']=[];
-		}
-		else
-		{
-			//**//
-		}
-		$panier=$_SESSION['panier'];
-		$produit = $_GET['id'];
-		$ses = $_POST['nombre_produit'];
-		
-		$_SESSION['panier'][$produit] =  $ses;
-		if($_POST['nombre_produit']==0)
-		{
-			unset($_SESSION['panier'][$produit]);
-		}
-		//affichage 
-		foreach($_SESSION['panier'] as $i =>$key) {
-			$i >0;
-			echo $i."=>".$key."<br>";	
-		}
-
+		//créer panier si n'existe pas
+		$_SESSION['panier']=[];
+	}
+	else
+	{
+		//**//
+	}
+	$panier=$_SESSION['panier'];
+	$produit = $_GET['id'];
+	$ses = $_POST['nombre_produit'];
+	$_SESSION['panier'][$produit] =  $ses;
+	if($_POST['nombre_produit']==0)
+	{
+		unset($_SESSION['panier'][$produit]);
+	}
+	//affichage 
+	foreach($_SESSION['panier'] as $i =>$key) {
+		$i >0;
+		echo $i."=>".$key."<br>";	
 	}
 }
 ?>
