823e186166c96168f10a195112033ce50f2f58ce
Kuinox <nvandeginste@gmail.com>
2017-05-31 00:24:33 +0200

    create-update bdd plus concurrentiel- retirer table inutile.

--- controller/CSV_FUNCTIONS/applyToAllProduct.php
+++ controller/CSV_FUNCTIONS/applyToAllProduct.php
@@ -4,8 +4,6 @@ include_once("../SQL/FUNCTIONS/updateProduct.php");
 include_once("../CSV_FUNCTIONS/getProduct.php");
 include_once("../CSV_FUNCTIONS/countLine.php");
 function applyToAllProduct($ressource, PDO $bdd, $columns, $code) {// run a function "code" on all the product of the CSV.
-    echo microtime(true);
-    ob_end_flush();
     $nb_product = countLine();
     $percent = floor($nb_product/10000);
     $bdd->query("SET GLOBAL innodb_flush_log_at_trx_commit = 0;") or die ("Erreur BDD");//optimisation
@@ -18,8 +16,10 @@ function applyToAllProduct($ressource, PDO $bdd, $columns, $code) {// run a func
     $bdd->beginTransaction();
     while($id<$nb_product && $id < 10000) { //!feof($ressource)
         if(($id+1)%$percent == 0) {
+            $percentage = 100*(round(($id+1)/$nb_product, 3));
+            $bdd->query("UPDATE bdd_status SET progress=$percentage");
             $bdd->commit();
-            echo 100*(round(($id+1)/$nb_product, 3))."\n";
+            echo $percentage."\n";
             flush();
             $bdd->beginTransaction();
         }
@@ -29,9 +29,9 @@ function applyToAllProduct($ressource, PDO $bdd, $columns, $code) {// run a func
         $code($bdd, $product, $prep);//injection magique
     }
     $bdd->commit();
+    $bdd->query("UPDATE bdd_status SET updating=false");
     $bdd->query("SET GLOBAL innodb_flush_log_at_trx_commit = 1;") or die ("Erreur BDD");
     $bdd->query("SET FOREIGN_KEY_CHECKS = 1;") or die ("Erreur BDD");
     $bdd->query("SET UNIQUE_CHECKS = 1;") or die ("Erreur BDD");
-    echo microtime(true);
 }
  ?>
--- controller/ajax/create_bdd.php
+++ controller/ajax/create_bdd.php
@@ -5,18 +5,9 @@ if (!isset($_SESSION['user']) || $_SESSION['user']['name_grade'] != 'admin') {
     echo "<h1>HTTP/1.1 403 Forbidden</h1>";
     exit;
 }
-/***********************************************
- * INIT                                        *
- ***********************************************/
-/***********************************************
- *Drop then create the DB.                     *
- ***********************************************/
-include_once("../CSV_FUNCTIONS/applyToAllProduct.php");
-include_once("../SQL/FUNCTIONS/sqlScriptInject.php");
-include_once("../SQL/FUNCTIONS/callThenReturn.php");
-include_once("../CSV_FUNCTIONS/openCSV.php");
-include_once("../CSV_FUNCTIONS/getProduct.php");
-include_once("../CSV_FUNCTIONS/sortNutriment.php");
+session_write_close();
+set_time_limit(0); //exec time can be long
+ob_end_flush();
 $dsn = "mysql:host=127.0.0.1; charset=utf8;";
 $user = "root";
 $password = "";
@@ -26,22 +17,61 @@ try {
     echo 'Connexion échouée : ' . $e->getMessage();
 }
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-set_time_limit(0); //exec time can be long
-foreach (scandir('../SQL/INIT') as $script) {
-    if($script != '.' && $script != '..') {
-        sqlScriptInject($bdd, "../SQL/INIT/".$script);
+try {
+    $data = $bdd->query("USE INFOOD;");
+    $data = $bdd->query("SELECT * FROM bdd_status");
+    if ($data->rowCount() == 0) {
+        $result['updating'] = false;
+    } else {
+        $result = $data->fetchAll(PDO::FETCH_ASSOC)[0];
     }
+
+} catch (PDOException $e) {
+    $result['updating'] = false;
 }
-$csv = openCSV();
-$columns = explode("\t",fgets($csv, 116528));
-$nutriments = [];
-$product = getProduct($csv, $columns);
-foreach (sortNutriment($product) as $nutriment) {
-    $result = callThenReturn($bdd, "CALL insert_nutriment('$nutriment', @output)");
-    $nutriments[$nutriment] = $result;
+
+
+
+if($result['updating']) {
+    while($result['updating']) {
+        $data = $bdd->query("SELECT * FROM bdd_status");
+        $result = $data->fetchAll(PDO::FETCH_ASSOC)[0];
+        echo $result['progress']."\n";
+        flush();
+        sleep(1);
+        //TODO
+    }
+
+} else {
+    /***********************************************
+     * INIT                                        *
+     ***********************************************/
+    /***********************************************
+     *Drop then create the DB.                     *
+     ***********************************************/
+    include_once("../CSV_FUNCTIONS/applyToAllProduct.php");
+    include_once("../SQL/FUNCTIONS/sqlScriptInject.php");
+    include_once("../SQL/FUNCTIONS/callThenReturn.php");
+    include_once("../CSV_FUNCTIONS/openCSV.php");
+    include_once("../CSV_FUNCTIONS/getProduct.php");
+    include_once("../CSV_FUNCTIONS/sortNutriment.php");
+
+    foreach (scandir('../SQL/INIT') as $script) {
+        if($script != '.' && $script != '..') {
+            sqlScriptInject($bdd, "../SQL/INIT/".$script);
+        }
+    }
+    $csv = openCSV();
+    $columns = explode("\t",fgets($csv, 116528));
+    $nutriments = [];
+    $product = getProduct($csv, $columns);
+    foreach (sortNutriment($product) as $nutriment) {
+        $result = callThenReturn($bdd, "CALL insert_nutriment('$nutriment', @output)");
+        $nutriments[$nutriment] = $result;
+    }
+    /***********************************************
+     * Injection                                   *
+     ***********************************************/
+    applyToAllProduct($csv, $bdd, $columns, 'injectProduct');
 }
-/***********************************************
- * Injection                                   *
- ***********************************************/
-applyToAllProduct($csv, $bdd, $columns, 'injectProduct', $prep);
 ?>
