13129f6c69d6baaad083a5028df2a9eef07235dc
Kuinox <nvandeginste@gmail.com>
2017-05-31 09:53:55 +0200

    Message si pas de résultats trouvé

--- model/functions/displayRecherche.php
+++ model/functions/displayRecherche.php
@@ -1,5 +1,8 @@
 <?php
 function displayRecherche(array $output) {
+    if(empty($output)) {
+        echo "Aucun résultat trouvé";
+    }
     $type = addslashes($_GET['type']);
     foreach ($output as $key => $value) {
         $id = array_shift($value);
