5a537be29fa4a419cb7dcf6fe68e8d14f23c2b3d
sirine <sassi@intechinfo.fr>
2017-06-01 11:15:37 +0200

    maintenant le supprimer compte ne s'affiche que pour admin

--- supprimer_utilisateur.php
+++ supprimer_utilisateur.php
@@ -1,12 +1,17 @@
 <?php
-include("view/supprimer_utilisateur.html");
+session_start();
 
-if(isset($_POST['supprimer']))
-{	
-	$nom=$_POST['nom'];
-	// echo $nom."<br>";
-	include("controller/functions/verifier_si_existe.php");
-	verifier_nom("$nom");
+if($_SESSION['user']['name_grade'] == 'admin') {
+
+	include("view/supprimer_utilisateur.html");
+
+	if(isset($_POST['supprimer']))
+	{
+		$nom=$_POST['nom'];
+		// echo $nom."<br>";
+		include("controller/functions/verifier_si_existe.php");
+		verifier_nom("$nom");
+	}
 }
 
 
