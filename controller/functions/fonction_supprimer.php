68648555290817e114241784e54317229f33ed62
sirine <sassi@intechinfo.fr>
2017-06-01 14:01:23 +0200

    mvc

--- controller/functions/fonction_supprimer.php
+++
@@ -1,8 +0,0 @@
-<?php
- function supprimerCompte(PDO $bdd, $nom)
-{
-	//modifier les donners par nulle
-	$requete = $bdd->query("UPDATE user SET email='nulle', pseudo='nulle', password='nulle',
-    height='0', weight='0' WHERE pseudo='$nom'");
-}
-?>
--- controller/functions/supprimerCompte.php
+++ controller/functions/supprimerCompte.php
@@ -2,7 +2,8 @@
  function supprimerCompte(PDO $bdd, $nom)
 {
 	//modifier les donners par nulle
-	$requete = $bdd->query("UPDATE user SET email='nulle', pseudo='nulle', password='nulle',
-    height='0', weight='0' WHERE pseudo='$nom'");
+
+	$requete = $bdd->query("UPDATE user SET email=NULL, pseudo=NULL, password=NULL,
+    height=NULL, weight=NULL WHERE pseudo='$nom'");
 }
 ?>
