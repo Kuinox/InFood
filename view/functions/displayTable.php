8e34004b9f0b5266397fe6832829cdfedaf14ab0
Deeeeenis <denis.audinet@gmail.com>
2017-06-02 15:55:26 +0200

    supprimer comment (pas fini )

--- view/functions/displayTable.php
+++ view/functions/displayTable.php
@@ -11,8 +11,9 @@ function displayNutri($nutri)
 {
   echo '<div class="nutriment">';
   echo "<table>";
+  var_dump($nutri);
   if (empty($nutri)) {
-    echo "Champs non renseigner";
+    echo "Champs non renseigné";
   }
   foreach ($nutri as $key => $value) {
     echo "<td>".$value['label']."</td><td>".$value['nutriment_quantity']."</td><tr>";
@@ -25,7 +26,7 @@ function displayAdditives($additives)
   echo '<div class="additives">';
   echo "additif : ";
   if (empty($additives)) {
-    echo "Champs non renseigner";
+    echo "Champs non renseigné";
   }
   foreach ($additives as $key => $value) {
       echo $value['label']. " ";
@@ -37,7 +38,7 @@ function displayBrand($brand)
   echo '<div class="brand">';
   echo "marque : ";
   if (empty($brand)) {
-    echo "Champs non renseigner";
+    echo "Champs non renseigné";
   }
   foreach ($brand as $key => $value) {
       echo $value['label']. " ";
@@ -49,7 +50,7 @@ function displayPackaging($packaging)
   echo '<div class="packaging">';
   echo "packaging : ";
   if (empty($packaging)) {
-    echo "Champs non renseigner";
+    echo "Champs non renseigné";
   }
   foreach ($packaging as $key => $value) {
       echo $value['label']. " ";
@@ -61,7 +62,7 @@ function displayPlace($place)
   echo '<div class="place">';
   echo "manufacturing_place : ";
   if (empty($place)) {
-    echo "Champs non renseigner";
+    echo "Champs non renseigné";
   }
   foreach ($place as $key => $value) {
       echo $value['label']. " ";
@@ -73,7 +74,7 @@ function displayAllergen($allergen)
   echo '<div class="allergen">';
   echo "allergene : ";
   if (empty($allergen)) {
-    echo "Champs non renseigner";
+    echo "Champs non renseigné";
   }
   foreach ($allergen as $key => $value) {
       echo $value['label']. " ";
