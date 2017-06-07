a9b692016d3ea494411570b4a5d27546475af76c
Kuinox <nvandeginste@gmail.com>
2017-06-08 01:27:33 +0200

    wip

--- user/preference.php
+++ user/preference.php
@@ -1,13 +1,13 @@
 <?php
 class Preference {
-
-
+    var $_bdd;
+    var $_pref;
     function __construct($bdd) {
         if(!isset($_SESSION['user'])) {
             throw new ErrorException("Preference can't be init if user is not connected");
         }
         $this->_bdd = $bdd;
-
+        
     }
 
     function getPref() {
@@ -20,7 +20,7 @@ class Preference {
                     WHERE id_user = ?";
         $prep = $this->_bdd->prepare($query);
         $prep->execute(array($_SESSION['user']['id_user']));
-        $prep->fetchAll(    );
+        $prep->fetchAll();
     }
 
     function addPref() {
