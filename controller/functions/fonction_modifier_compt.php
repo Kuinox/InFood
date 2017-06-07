72a840a03fdbf837bac259c73423d2dcd2d52989
Kuinox <nvandeginste@gmail.com>
2017-06-07 11:09:33 +0200

    fix d'un bug qui aurait été embettant

--- controller/functions/fonction_modifier_compt.php
+++ controller/functions/fonction_modifier_compt.php
@@ -2,7 +2,7 @@
  function updateUser(PDO $bdd)
 {
     //modifier les donners par nulle
-	$prep = $bdd->prepare("UPDATE user SET pseudo=? password=? email=? height=? weight=?");
-    $prep->execute(array($_SESSION['pseudo'], hash("sha256", $_SESSION['password']), $_SESSION['email'], $_SESSION['height'], $_SESSION['weight']));
+	$prep = $bdd->prepare("UPDATE user SET pseudo=? password=? email=? height=? weight=? WHERE id_user = ?");
+    $prep->execute(array($_SESSION['pseudo'], hash("sha256", $_SESSION['password']), $_SESSION['email'], $_SESSION['height'], $_SESSION['weight'], $_SESSION['id_user']));
 }
 ?>
