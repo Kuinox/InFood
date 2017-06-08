c79470be21be31bf9bed851fbdfd72c51fbde620
sirine <sassi@intechinfo.fr>
2017-06-08 15:43:36 +0200

    kl

--- model/adminModif.php
+++ model/adminModif.php
@@ -1,42 +1,63 @@
-<form action="adminModif" method="post">
-<input type="text" name="name" placeholder="nom du personne pour modifier"/>
-<input type="submit" name="cher" value="Changer"/>
-</form>
 <?php
-session_start();
-//include("..../SQL/FUNCTIONS/connect.php");//connexion au bdd
-if(isset($_POST["cher"]))
-{$login = $_POST['name'];//protège les chars pour l'utiliser  dans une requête SQL
+include("../view/adminModifvue1.php");
+//session_start();
+//include("../controller/SQL/FUNCTIONS/connect.php");//connexion au bdd
 $bdd=mysqli_connect("localhost","root","","infood");
-$query = "    SELECT
-            u.id_user,
-            u.pseudo,
-            u.password,
-            u.email,
-            u.height,
-            u.weight,
-            g.name_grade
-            FROM
-              USER u
-            JOIN
-              grade_user gu
-            ON
-              u.id_user = gu.user_id_user
-            JOIN
-              grade g
-            ON
-              gu.grade_id_grade = g.id_grade
-              WHERE u.pseudo= ?)";
-$prep = $bdd->prepare($query);
 
-$prep->execute(array($login)) or die ("Erreur BDD");
+if(isset($_POST['cher'])){
+$login = $_POST['name'] ;//protège les chars pour l'utiliser  dans une requête SQL
 
-if($prep->rowCount()>0) {//s'i nom existe
-    $data = $prep->fetch(PDO::FETCH_ASSOC); //Récupère la ligne suivante d'un jeu de résultats PDO
-    $_SESSION['userAd']= $data;
-    echo "sucess";
-} else { //si le mot de passe et email n'existe pas -> connexion échoué
-    echo "wrong"; //affiche alerte
+$sel_user = "select * from user where pseudo='$login'";
+$run_user = mysqli_query($bdd, $sel_user);
+
+//recherche le nombre de fois le email et mot de passe existe
+$check_user = mysqli_num_rows($run_user);
+
+//s'il existe mot de passe et email
+//connection réussi
+if($check_user>0)
+{
+  //nouvelle connection à la base (necessiare)
+  $bdd = new PDO('mysql:dbname=infood;host=localhost','root','');
+
+  //recherche dans la bdd sur le nom
+  $requete = $bdd->query("SELECT
+              u.id_user,
+              u.pseudo,
+              u.password,
+              u.email,
+              u.height,
+              u.weight,
+              g.name_grade
+              FROM
+                USER u
+              JOIN
+                grade_user gu
+              ON
+                u.id_user = gu.user_id_user
+              JOIN
+                grade g
+              ON
+                gu.grade_id_grade = g.id_grade
+                WHERE u.pseudo=$login");
+
+  while($data = $requete->fetch())
+		{
+			// echo'<h2>'.$data['pseudo'].'</h2><br>';
+
+			//mettre le nom dans une session
+			$_SESSION['user2']=$data;
+      var_dump($_SESSION['user2']);
+
+		}
+echo "oui";
+//si le mot de passe et email n'existe pas.
+//connection échouer
+}else
+{
+  //affiche alert
+  echo "non";
 }
-var_dump($_SESSION['userAd']);
-}?>
+}
+
+?>
---
+++ model/adminModifvue1.php
@@ -0,0 +1,4 @@
+<form action='adminModif' method="post">
+<input type="text" name="name" placeholder="nom du personne pour modifier"/>
+<input type="submit" name="cher" value="Changer"/>
+</form>
