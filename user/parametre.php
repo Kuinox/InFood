1a707b32f0c1c428f70d7d204b1dbe0305a5c789
Deeeeenis <denis.audinet@gmail.com>
2017-06-08 10:01:12 +0200

    fix

--- user/parametre.php
+++ user/parametre.php
@@ -4,20 +4,20 @@ include(__DIR__."/../controller/SQL/FUNCTIONS/connect.php");
 include(__DIR__."/modifierCompte.php");
 var_dump($_SESSION['user']);
 if(isset($_POST['action'])) {
-    $var = $_POST['action'];
-	if($var == "password") {
-			hash("sha256", $_SESSION['password']);
-	} else {
-    $prep = $bdd->prepare ("SELECT id_user FROM user WHERE email=? OR pseudo = ?");
-    $prep->execute (array($_SESSION['user']['email'], $_SESSION['user']['pseudo']));
-    if ($prep->rowCount()<2) {
-		$_SESSION['user'][$var] = $_POST[$var];
-		updateUser($bdd);
-    }else{
-    echo "pseudo ou email deja utiliser";
-  }
-  }
-  var_dump($_SESSION['user']);
+        $var = $_POST['action'];
+    if($var == "password") {
+            hash("sha256", $_SESSION['password']);
+    } else {
+        $prep = $bdd->prepare ("SELECT id_user FROM user WHERE email=? OR pseudo = ?");
+        $prep->execute (array($_SESSION['user']['email'], $_SESSION['user']['pseudo']));
+        if ($prep->rowCount()>=1) {
+            echo "pseudo ou email deja utiliser";
+        }else{
+            $_SESSION['user'][$var] = $_POST[$var];
+            updateUser($bdd);
+    }
+    }
+    var_dump($_SESSION['user']);
 } else {
 
 }
