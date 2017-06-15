897cea5e3ee9d21970cad5a210d5f996abf99561
sirine <sassi@intechinfo.fr>
2017-06-15 09:39:17 +0200

    p

--- controller/supprimerMonCompte.php
+++ controller/supprimerMonCompte.php
@@ -1,5 +1,12 @@
 <?php
 include("../controller/SQL/FUNCTIONS/connect.php");
+
+ function supprimerCompte(PDO $bdd, $nom)
+{
+	$requete = $bdd->query("UPDATE user SET email=NULL, pseudo=NULL, password=NULL,
+  height=NULL, weight=NULL WHERE pseudo='$nom'");
+}
+
 session_start();
 if(isset($_SESSION['user'])){
   if($_SESSION['user']['name_grade'] == 'utilisateur') {
@@ -7,7 +14,9 @@ if(isset($_SESSION['user'])){
     $nom=$_SESSION['user']['pseudo'];
     supprimerCompte($bdd, $nom);
     session_destroy();
-    header('Location:/infood2/index.php');
+    echo"session destroy";
+    //header('Location:/infood2/index.php');
   }
 }
+
 ?>
