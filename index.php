1bd1678e228991658b32a87498a6a317f0d3d82a
Kuinox <nvandeginste@gmail.com>
2017-06-26 20:03:11 +0200

    traaavaiiillll

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 include("model/top.php");
 include("model/functions/filtres.php");
+include("controller/SQL/functions/connect.php");
 addFilter($bdd, $_SESSION['user']['id_user'], "{'nom':'okaylenommarche'}", "SELECT * FROM `user`", "nope", "#FF00FF");
+displayAllFilters($bdd);
 include("model/bot.php");
 ?>
