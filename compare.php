a61fa8cd3070f7a3b7a78a4b4b0efbb195653d59
Deeeeenis <denis.audinet@gmail.com>
2017-06-29 23:11:33 +0200

    comp

--- compare.php
+++ compare.php
@@ -3,33 +3,59 @@ include('model/top.php');
 include("controller/SQL/FUNCTIONS/connect.php");
 include("controller/SQL/FUNCTIONS/infoAliment.php");
 include("controller/SQL/FUNCTIONS/select.php");
-include("view/functions/displayCompare.php");
-var_dump($_SESSION['compare']);
-echo "<div class='comparaison>'";
-echo "<table>";
-echo "<table class='nutri2'>
-        <th>Nutriments</th>";
+echo "<div class='comparaison'>";
+echo "<table class = 'nutri1'>";
+echo "<th>Nutriments</th>";
 $nutri = [];
+$additives = [];
 $nb = count($_SESSION['compare']);
 for ($i = 0 ;$i < $nb; $i++) {
     $result = select ($bdd, "aliment", "id_aliment",$_SESSION['compare'][$i]);
-    echo "<th>Quantité dans ".$result[0]['name_aliment']."</th>";
+    echo "<th>".$result[0]['name_aliment']."</th>";
 }
 foreach ($_SESSION['compare'] as $key => $value) {
         $nutri[] = nutriments($bdd, $value);
 }
-foreach ($nutri[0] as $key0 => $val) {
+foreach ($nutri[0] as $key => $val) {
      echo "<tr><td>".$val['label']."</td>";
         foreach($nutri as $clee => $valeur) {
-            if (!isset($valeur[$key0]['nutriment_quantity'])) {
-                $valeur[$key0]['nutriment_quantity'] = "";
+            if (!isset($valeur[$key]['nutriment_quantity'])) {
+                $valeur[$key]['nutriment_quantity'] = "non renseigné";
             }
-            echo "<td>".$valeur[$key0]['nutriment_quantity']."</td>";
+            echo "<td>".$valeur[$key]['nutriment_quantity']."</td>";
     }
     echo "</tr>";
 }
-// $nutri = nutriments($bdd, $value);
-// $additives = additives($bdd, $value);
+echo "</table>";
+foreach ($_SESSION['compare'] as $key => $value) {
+    $additives[] = additives($bdd, $value);
+}
+echo "<table class= 'nutri3'>";
+for ($i = 0 ;$i < $nb; $i++) {
+    $result = select ($bdd, "aliment", "id_aliment",$_SESSION['compare'][$i]);
+    echo "<th>".$result[0]['name_aliment']."</th>";
+}
+
+$nbmax = 0;
+    foreach ($additives as $key => $value) {
+        $nbmax2 = count($additives[$key]);
+        if ($nbmax < $nbmax2){
+            $nbmax = $nbmax2;
+        }
+    }
+    for ($i=0; $i < $nbmax ; $i++) {
+         echo "<tr>";
+        foreach ($additives as $clee => $val) {
+            if (!isset($val[$i])) {
+                echo "<td> //      </td>";
+            }else {
+                echo "<td>".$val[$i]['name']."</td>";
+            }
+
+        }
+    }
+        echo "</tr>";
+
 // $brand = brand($bdd,$value);
 // $packaging = packaging($bdd,$value);
 // $place = manufact_place($bdd,$value);
