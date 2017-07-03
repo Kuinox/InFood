1b42be85827d2f6346683a8ead97a9dfa5cf3d5f
Deeeeenis <denis.audinet@gmail.com>
2017-07-03 14:53:53 +0200

    petit ajout

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 normalHidden<?php
 include("model/top.php");
+?>
+<div class="index">
+    <p> In'Food est un site d'information à caractère ludique, il vise à informer les usagé sur le contenu des aliments ou produits consommables que l’on peut retrouver en magasin toutefois nous ne garantissons pas la véracité des informations présentes.Nos informations provienne d'une API que l'on peut trouver sur <a href="https://fr.openfoodfacts.org/">open food facts </a>   </p>
+<?php
 include("model/bot.php");
 ?>
